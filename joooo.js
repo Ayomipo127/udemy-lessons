4180f8624dcee23bb1d833a906f15b30aafebe13
Ayomipo127 <81769230+Ayomipo127@users.noreply.github.com>
2022-07-24 04:30:29 +0100

    my work in progress

---
+++ joooo.js
@@ -0,0 +1,51 @@
+
+/**object orietend programming */
+
+var customer = {
+	checking: 0,
+	savings: 0,
+	deposit: function (amount) {
+		customer.checking += amount;
+	},
+	tranfer: function (amount) {
+		customer.checking -= amount;
+		customer.savings += amount;
+	}
+	
+};
+
+customer.deposit(1000);
+customer.tranfer(200);
+
+console.log('Checking:' + customer.checking);
+console.log('Savings:' + customer.savings);
+
+const bodyElement = document.getElementById("parent");
+const paragraph = document.getElementById("child");
+bodyElement.removeChild(paragraph);
+
+function sayHello() {
+	document.getElementById("prompt").innerHTML = "BAD GUY DOINGS";
+}
+
+
+function submit() {
+	const surname = document.getElementById("street").value;
+
+	document.getElementById("prompt").innerHTML = surname + " is your street!";
+}
+
+var counter = 3;
+
+function upvote() {
+
+	counter = counter + 1;
+	document.getElementById("votes").innerHTML = counter + " votes"
+}
+
+
+function downvote() {
+
+	counter = counter - 1;
+	document.getElementById("votes").innerHTML = counter + " votes"
+}
