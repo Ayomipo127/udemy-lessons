d22049178f91459577628475b69e82fbebe2ba73
Ayomipo127 <81769230+Ayomipo127@users.noreply.github.com>
2022-08-13 05:16:56 +0100

    age calculator
    calculator

--- script.js
+++ script.js
@@ -50,24 +50,30 @@ function downvote() {
 	document.getElementById("votes").innerHTML = counter + " votes"
 }
 
-function showAttribute() {
-	const el = document.querySelector("img");
-	el.src = "./images/IMG_20210514_115225_701.jpg";
+const calculateAge = () => {
+	let d1 = document.getElementById("date").value;
+	let m1 = document.getElementById("month").value;
+	let y1 = document.getElementById("year").value;
+
+	let date = new Date();
+	let d2 = date.getDate();
+	let m2 = 1 + date.getMonth();
+	let y2 = date.getFullYear();
+	let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+	if (d1 > d2) {
+		d2 = d2 + month[m2 - 1]
+		m2 = m2 - 1;
+	}
+
+	if (m1 > m2) {
+		m2 = m2 + 12;
+		y2 = y2 - 1;
+	}
+	let d = d2 - d1;
+	let m = m2 - m1;
+	let y = y2 - y1;
+
+
+	document.getElementById("displayAge").innerText = 'your Age is ${y} Years, ${m} Months and $ Days';
 }
-
-
-function updateValue() {
-	log.innerText = input.value;
-}
-
-var input = document.querySelector('input');
-var log = document.getElementById('log');
-input.onchange = updateValue;
-
-
-function changeColor() {
-	item.classList.toggle("pink");
-}
-
-const item = document.querySelector("h2");
-item.addEventListener("click", changeColor);
